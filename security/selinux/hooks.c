817f3836627b5de9cc29c2683d8cc47fc25a2f54
Mark Salyzyn <salyzyn@google.com>
2015-01-07 09:27:15 -0800

    pstore: selinux: add security in-core xattr support for pstore and debugfs
    - add "pstore" and "debugfs" to list of in-core exceptions
    - change fstype checks to boolean equation
    - change from strncmp to strcmp for checking
    
    (Cherry Pick from commit 2294d499b7969df3838becf5e58bf16b0e3c86c8)
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 18917345
    Bug: 18935184
    Change-Id: Ib648f30ce4b5d6c96f11465836d6fee89bec1c72
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>
    
    Conflicts:
    	security/selinux/hooks.c

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -442,7 +442,6 @@ static int sb_finish_set_opts(struct super_block *sb)
 	 * setting SELinux context on in-core inodes.
 	 */
 	if (strncmp(sb->s_type->name, "rootfs", sizeof("rootfs")) == 0)
-		sbsec->flags |= SE_SBLABELSUPP;
 
 	/* Initialize the root inode. */
 	rc = inode_doinit_with_dentry(root_inode, root);
