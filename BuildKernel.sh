ed45155751413177333941a4f151bf05590c0ad1
GMCadiom <gm.cadiom@gmail.com>
2017-04-11 02:57:40 +0530

    Travis-CI : Add TCI Support

--- BuildKernel.sh
+++ BuildKernel.sh
@@ -6,7 +6,7 @@
 mkdir -p out
 
 #Change toolchain path before using build script!
-#export CROSS_COMPILE=~/toolchains/arm-eabi-linaro-4.7.3/bin/arm-eabi-
+export CROSS_COMPILE=$PWD/toolchain/bin/arm-eabi-
 #Using toolchain in Travis
 
 #Enable when needed:
